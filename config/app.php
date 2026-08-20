6005e0cbef5469cd444dbea113c93af774b47051
MUHAMMAD RANDY KARNA <muhammadrandykarna@gmail.com>
2026-08-20 09:19:37 +0700

    chore: update base_url in app configuration to reflect project directory change

--- config/app.php
+++ config/app.php
@@ -5,7 +5,7 @@
 return [
     'app_name'   => 'TK IT Quantum School',
     // Sesuaikan jika folder project diletakkan di dalam subfolder htdocs, contoh: '/tkitquantum'
-    'base_url'   => '/tkitquantum/public',
+    'base_url'   => '/tkitquantum2/public',
     'env'        => 'development', // development | production
     'session_lifetime' => 1800,    // 30 menit auto logout
     'login_max_attempts' => 5,
