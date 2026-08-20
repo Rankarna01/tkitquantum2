13f37b4b4805b359a6cc01d4261262de316165c9
MUHAMMAD RANDY KARNA <muhammadrandykarna@gmail.com>
2026-08-20 13:17:28 +0700

    chore: update database name and user credentials in configuration

--- config/database.php
+++ config/database.php
@@ -5,8 +5,8 @@
  */
 return [
     'host'    => '127.0.0.1',
-    'dbname'  => 'u117434194_tkit',
-    'user'    => 'u117434194_tkit',
+    'dbname'  => 'u117434194_tkit2',
+    'user'    => 'u117434194_tkit2',
     'pass'    => 'Randy2005_',
     'charset' => 'utf8mb4',
 ];
